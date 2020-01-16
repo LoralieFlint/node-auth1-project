3ee62d6a23516e1bb5f1ff145352da1c4dd079cb
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-01-15 17:06:31 -0700

    MVP met finally got router working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const userRouter = require("./users/users-router");
 
 const server = express();
 const port = process.env.PORT || 5000;
@@ -9,10 +10,12 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.get("/", (req, res, next) => {
+server.use("/", userRouter);
+
+server.get("/api", (req, res, next) => {
   res.json({ message: "welcome to my API" });
 });
-
+  
 server.use((err, req, res, next) => {
   console.log("error:", err);
 
